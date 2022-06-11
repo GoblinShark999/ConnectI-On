19568440974f29cdaefc4aef18ec757adf679af5
Ali <amadams12336@gmail.com>
2022-06-11 18:57:48 -0500

    added Login functionality

---
+++ client/components/app.jsx
@@ -0,0 +1,104 @@
+<<<<<<< Updated upstream
+=======
+import React, {useEffect, useState} from 'react';
+//import './stylesheets/styling.scss';
+import {Box, Input, Button} from '@mui/material';
+
+function App(props) {
+    const [userData, setUserData] = useState({
+        username: '',
+        password: '',
+        location: ''
+    });
+    const [isNewUser, setIsNewUser] = useState(false);
+    return (
+        <div>
+            <Login userData={userData} setUserData={setUserData} isNewUser={isNewUser} setIsNewUser={setIsNewUser}/>
+        </div>
+
+    )
+
+}
+export default App;
+
+function handleNewUserInput(e, userData, setUserData) {
+    setUserData(() => ({...userData, [e.target.name]: e.target.value}));
+}
+
+const Login = (props) => {
+    if (!props.isNewUser) {
+        return (
+            <form>{/*//onSubmit={handleSubmit()}>*/}
+                <label>
+                    Username:
+                    <input type="text" name='username' value={props.userData.username} onChange={(event) => {handleNewUserInput(event, props.userData, props.setUserData)}}/>
+                    Password:
+                    <input type="text" name='password' value={props.userData.password} onChange={(event) => {handleNewUserInput(event, props.userData, props.setUserData)}}/>
+                    <Button style={{backgroundColor: "red"}} type="submit" value="Log in" onClick={() => {
+                    }}/>
+                    <Button style={{backgroundColor: "blue"}} type="submit" value="Sign Up" onClick={() => {
+                        props.setIsNewUser(!props.isNewUser);
+                    }}/>
+                </label>
+            </form>
+        )
+    } else {
+        return (<Box>
+                Username:
+                <input type="text" name='username' value={props.userData.username} onChange={(e) => {handleNewUserInput(e, props.userData, props.setUserData)}}/>
+                Password:
+                <input type="text" name='password' value={props.userData.password} onChange={(e) => {handleNewUserInput(e, props.userData, props.setUserData)}}/>
+                Location:
+                <input type="text" name='location' value={props.userData.location} onChange={(e) => {handleNewUserInput(e, props.userData, props.setUserData)}}/>
+                <Button style={{backgroundColor: "green"}} type="submit" value="Signed in" onClick={() => {
+                }}/>
+            </Box>
+        )
+    }
+}
+
+
+function handleSubmit(e) {
+
+}
+// <button
+// onClick = {(event)
+// =>
+// setUserData({...userData, [event.target.name]: event.target.value})
+// }><
+// /button>
+
+// const [value, setValue] = React.useState('');
+
+// function handleChange(e){
+
+//   setValue(e.target.value);
+//   //console.log(value);
+// }
+
+// function handleSubmit(e){
+//   props.add_market(value);
+//   setValue('');
+//   //console.log(props);
+//   //this.setState({value: event.target.value});
+
+//   e.preventDefault();
+// }
+
+/* <form onSubmit={handleSubmit}>
+        <label>
+          Location:
+          <input type="text" value = {value} onChange={handleChange}/>
+        </label>
+        <input type="submit" value="Add Market" />
+      </form> */
+
+
+// const [allValues, setAllValues] = useState({
+//   mobile: '',
+//   username: '',
+//   email: '',
+//   password: '',
+//   confirmPassword: ''
+// });
+>>>>>>> Stashed changes
