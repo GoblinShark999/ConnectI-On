63b6528f59635c3fcba07140dbd6e239ce9b9d79
Peng Dong <dongpeng101@gmail.com>
2022-06-11 14:40:31 -0400

    Peng: adding login page

--- client/index.js
+++ client/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
 import App from './components/app.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
-import styling from './stylesheets/styling.scss' //client/stylesheets/styling.scss
+// import styling from './stylesheets/styling.scss'; //client/stylesheets/styling.scss
 
-render(
-    <App />,
-    document.getElementById('root')
-);
+// render(
+//     <App />,
+//     document.getElementById('root')
+// );
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
