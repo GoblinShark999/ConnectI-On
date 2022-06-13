6d863437e39a5f2fb9a0ccf60868601a6aa55ad5
Ali <amadams12336@gmail.com>
2022-06-13 10:53:39 -0500

    organized Login component to separate page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 //import './stylesheets/styling.scss';
-import {Box, Input, Button} from '@mui/material';
 
 
 function App(props) {
@@ -20,38 +19,5 @@ function App(props) {
 }
 export default App;
 
-function handleNewUserInput(e, userData, setUserData) {
-    setUserData(() => ({...userData, [e.target.name]: e.target.value}));
-}
 
-const Login = (props) => {
-    if (!props.isNewUser) {
-        return (
-            <form>{/*//onSubmit={handleSubmit()}>*/}
-                <label>
-                    Username:
-                    <input type="text" name='username' value={props.userData.username} onChange={(event) => {handleNewUserInput(event, props.userData, props.setUserData)}}/>
-                    Password:
-                    <input type="text" name='password' value={props.userData.password} onChange={(event) => {handleNewUserInput(event, props.userData, props.setUserData)}}/>
-                    <Button style={{backgroundColor: "red"}} type="submit" value="Log in" onClick={() => {
-                    }}/>
-                    <Button style={{backgroundColor: "blue"}} type="submit" value="Sign Up" onClick={() => {
-                        props.setIsNewUser(!props.isNewUser);
-                    }}/>
-                </label>
-            </form>
-        )
-    } else {
-        return (<Box>
-                Username:
-                <input type="text" name='username' value={props.userData.username} onChange={(e) => {handleNewUserInput(e, props.userData, props.setUserData)}}/>
-                Password:
-                <input type="text" name='password' value={props.userData.password} onChange={(e) => {handleNewUserInput(e, props.userData, props.setUserData)}}/>
-                Location:
-                <input type="text" name='location' value={props.userData.location} onChange={(e) => {handleNewUserInput(e, props.userData, props.setUserData)}}/>
-                <Button style={{backgroundColor: "green"}} type="submit" value="Signed in" onClick={() => {
-                }}/>
-            </Box>
-        )
-    }
-}
+
